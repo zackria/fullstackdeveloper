2e7126a6380179fd386d31a32d151794a6086f05
Zack Dawood <zackria@gmail.com>
2022-10-16 07:26:35 -0400

    Update SearchArray01.java

--- 02 Java/Exercises/01_LoopProject/SearchArray01.java
+++ 02 Java/Exercises/01_LoopProject/SearchArray01.java
@@ -2,5 +2,13 @@ class SearchArray01{
 	public static void main(String[] args) {
 		//creating an instance of an array  
 		int[] arr = new int[] {78, 34, 1, 3, 90, 34, -1, -4, 6, 55, 20, -65}; 
+		int searchValue = 90;
+
+		for (int i = 0; i < arr.length; i++)   
+		{  
+			if(searchValue==arr[i]){
+				System.out.println("Search value "+searchValue+" found at position "+i);
+			}
+		}
 	}
 }
